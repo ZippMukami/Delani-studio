762ca1fb5e4adeffdb18b9be734da7bc2db07c23
ZippMukami <zimukami@gmail.com>
2022-03-13 22:33:30 +0300

    This commit adds the image title overlay on the portfolio.

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function (){
+
+$(".item-wrap").hover(function(){
+    $(this).find(".image-title").fadeIn(300);
+}, function(){
+    $(this).find(".image-title").fadeOut(100);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
