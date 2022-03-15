18ade574263070f80ed6894d808faf95345ba98c
ZippMukami <zimukami@gmail.com>
2022-03-15 08:33:17 +0300

    This commit validates the form.

--- js/script.js
+++ js/script.js
@@ -51,6 +51,30 @@ $(document).ready(function () {
 
     )
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // let email = $(".required email").value;
     // let name = $("#mce-NAME").value;
     // let message = $("#mce-MESSAGE").value;
@@ -95,22 +119,30 @@ $(document).ready(function () {
     //     var $mcj = jQuery.noConflict(true);
 
 
-        // userOutput.innerHTML = name + ", we have received your message. Thankyou for reaching out to us."
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // userOutput.innerHTML = name + ", we have received your message. Thankyou for reaching out to us."
 
 
 });
+
+
+document.querySelector("#button").addEventListener("click", function (e) {
+    e.preventDefault();
+});
+
+
+function send() {
+    let name = document.getElementById("form4Example1").value;
+    let email = document.getElementById("form4Example2").value;
+    let message = document.getElementById("form4Example3").value;
+
+
+    if (name === "") {
+        alert("Kindly enter your name!");
+    } else if (email === "") {
+        alert("Kindly enter your email!");
+    } else if (message === "") {
+        alert("Kindly write a message!");
+    } else {
+        alert("Hi " + name + ", we have received your message. Thank you for reaching out to us.")
+    }
+};
